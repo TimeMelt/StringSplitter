a8dc13fa701104c3f31d134f0ff14b7ceb51d60d
TimeMelt <jeanpaulstevens50@gmail.com>
2022-07-29 10:23:35 -0400

    refactor plus tweaks

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # StringSplitter
 Split Strings into 4 character sequences and output to files 
 
+## Instuctions
+  - clone/download this repo
+  - make sure split.rb is in the same directory as intended input file (input file must be named "input.txt")
+  - run command "ruby split.rb"
+
 ## Notes
   - input file must be named "input.txt" (initial input file)
   - output to words file is "words.txt" (before being split into 4 character sequences)
   - output to sequences file is "sequences.txt" (after being split into 4 character sequences)
+  - this repo contains a sample input file (input.txt) along with the corresponding output files (words.txt, sequences.txt)
+  - output files (words.txt, sequences.txt) are automatically created if the files do not already exist
--- split.rb
+++ split.rb
@@ -2,45 +2,59 @@
 # Words to Sequences Splitter
 
 class StringSplit
-    
-    # read input dictionary file "input.txt"
-    def readFile()
-        arr = IO.readlines("input.txt")
-        return arr
-    end 
+
+    @wordData # array to store words.txt data
+    @seqData # array to store sequences.txt data
+
+    # init word and sequence
+    def initialize()
+        @wordData = Array.new
+        @seqData = Array.new
+    end
 
     # function to write words to output file (words.txt)
-    def writeWordOutput(inputData)
+    def writeWordOutput()
+    
+        # read input file, 
+        File.open("input.txt") do |file|
+           file.each_line do |words|
+              words = words.downcase 
+              words.gsub!(/[^a-z]/, '') # remove numbers and special characters
+              @wordData.push(words) # store new string into array "newData"
+            end
+        end    
+
+        @wordData = @wordData.reject { |chars| chars.empty? || chars.length < 4 } # remove blank entries and entries under 4 characters
+        @wordData = @wordData.uniq # remove duplicate sequences
+
         aFile = File.open("words.txt", "w+") do |f|
-            f.puts(inputData)
+            f.puts(@wordData)
         end
     end
 
     # create 4 character sequences and write to output file (sequences.txt)
-    def writeSeqOutput(inputData)
-
-        newData = Array.new # create array to store new strings 
+    def writeSeqOutput()
     
         # read input file, 
         File.open("input.txt") do |file|
            file.each_line(4) do |characters| # divide strings into 4 character pieces
-              characters.gsub!(/[^A-Za-z]/, '') # remove numbers and special characters
-              newData.push(characters) # store new string into array "newData"
+              characters = characters.downcase 
+              characters.gsub!(/[^a-z]/, '') # remove numbers and special characters
+              @seqData.push(characters) # store new string into array "newData"
             end
         end    
         
-        newData = newData.reject { |c| c.empty? || c.length < 4 } # remove blank entries and entries under 4 characters
-        newData = newData.uniq # remove duplicate sequences
+        @seqData = @seqData.reject { |chars| chars.empty? || chars.length < 4 } # remove blank entries and entries under 4 characters
+        @seqData = @seqData.uniq # remove duplicate sequences
 
         # write sequences to output file (sequences.txt)
         aFile = File.open("sequences.txt", "w+") do |f|
-            f.puts(newData)
+            f.puts(@seqData)
         end
     end
    
 end
 
 SS = StringSplit.new
-inputData = SS.readFile() # read and store input dictionary file "input.txt"
-SS.writeWordOutput(inputData) # write to "words.txt" output file
-SS.writeSeqOutput(inputData) # write to "sequences.txt" output file
+SS.writeWordOutput() # write to "words.txt" output file
+SS.writeSeqOutput() # write to "sequences.txt" output file
